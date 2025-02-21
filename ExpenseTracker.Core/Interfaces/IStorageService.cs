d5ce6c14b19ff963381fe6fda673f925987cc5f4
Abror Khalilov <akhalinem@gmail.com>
2025-02-21 13:40:40 +0500

    refactor(storage): remove IStorageService and JsonStorageService implementations

--- ExpenseTracker.Core/Interfaces/IStorageService.cs
+++
@@ -1,9 +0,0 @@
-using ExpenseTracker.Core.Models;
-
-namespace ExpenseTracker.Core.Interfaces;
-
-public interface IStorageService<T>
-{
-    Result<IEnumerable<T>> Load();
-    Result<bool> Save(IEnumerable<T> items);
-}
