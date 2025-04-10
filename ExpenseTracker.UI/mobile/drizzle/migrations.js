b0ba4c6ba286f7e2645eee7dfebe16d3f54d44bc
akhalinem <akhalinem@gmail.com>
2025-04-11 02:11:24 +0500

    refactor: rename previous migration scripts

--- ExpenseTracker.UI/mobile/drizzle/migrations.js
+++ ExpenseTracker.UI/mobile/drizzle/migrations.js
@@ -1,13 +1,13 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_whole_sharon_carter.sql';
+import initial from './0000_initial.sql';
 import addIncomesTable from './0001_add_incomes_table.sql';
 
 export default {
   journal,
   migrations: {
-    m0000,
+    initial,
     addIncomesTable
   }
 }
