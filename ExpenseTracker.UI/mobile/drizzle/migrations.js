71fa75dddb022ee06edef3416a07f44d93bc4d7f
akhalinem <akhalinem@gmail.com>
2025-04-12 00:12:41 +0500

    fix: fix broken migrations

--- ExpenseTracker.UI/mobile/drizzle/migrations.js
+++ ExpenseTracker.UI/mobile/drizzle/migrations.js
@@ -1,13 +1,13 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import initial from './0000_initial.sql';
-import addIncomesTable from './0001_add_incomes_table.sql';
+import m0000 from './0000_initial.sql';
+import m0001 from './0001_add_incomes_table.sql';
 
 export default {
   journal,
   migrations: {
-    initial,
-    addIncomesTable
+    m0000,
+    m0001
   }
 }
