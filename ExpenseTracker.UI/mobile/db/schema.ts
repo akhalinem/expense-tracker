8c437bfb2e4b7d7472824ca90c711d881657ddd1
akhalinem <akhalinem@gmail.com>
2025-04-12 00:44:31 +0500

    chore: add `transactions` and `transaction_types` tables

--- ExpenseTracker.UI/mobile/db/schema.ts
+++ ExpenseTracker.UI/mobile/db/schema.ts
@@ -1,4 +1,4 @@
-import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, real, sqliteTable, text, } from "drizzle-orm/sqlite-core";
 
 export const categoriesTable = sqliteTable('categories', {
     id: int().primaryKey({ autoIncrement: true }),
@@ -20,8 +20,24 @@ export const budgetsTable = sqliteTable('budgets', {
     amount: real().notNull(),
 })
 
+export const transactionTypesTable = sqliteTable('transaction_types', {
+    id: int().primaryKey({ autoIncrement: true }),
+    name: text().notNull(),
+});
+
+export const transactionsTable = sqliteTable('transactions', {
+    id: int().primaryKey({ autoIncrement: true }),
+    typeId: int().notNull().references(() => transactionTypesTable.id),
+    amount: real().notNull(),
+    date: text().notNull(),
+    description: text(),
+    categoryId: int().references(() => categoriesTable.id),
+});
+
 export const schema = {
     budgets: budgetsTable,
     categories: categoriesTable,
     expenses: expensesTable,
+    transactionTypesTable,
+    transactions: transactionsTable,
 };
