a7c18c661a6a18aba60d8bec0b0d0550f6799338
akhalinem <akhalinem@gmail.com>
2025-05-19 01:13:24 +0500

    fix: invalidate categories query on category creation, update, and deletion

--- ExpenseTracker.UI/mobile/components/CategoryForm.tsx
+++ ExpenseTracker.UI/mobile/components/CategoryForm.tsx
@@ -40,6 +40,7 @@ export default function CategoryForm({
             categoriesService.createCategory({ name, color }),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["categoriesWithTransactionsCount"] });
+            queryClient.invalidateQueries({ queryKey: ["categories"] });
             onSuccess();
         }
     });
@@ -50,6 +51,7 @@ export default function CategoryForm({
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["categoriesWithTransactionsCount"] });
             queryClient.invalidateQueries({ queryKey: ["transactions"] });
+            queryClient.invalidateQueries({ queryKey: ["categories"] },);
             onSuccess();
         }
     });
@@ -58,6 +60,7 @@ export default function CategoryForm({
         mutationFn: (id: number) => categoriesService.deleteCategory(id),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["categoriesWithTransactionsCount"] });
+            queryClient.invalidateQueries({ queryKey: ["categories"] });
             onSuccess();
         },
         onError: (error) => {
