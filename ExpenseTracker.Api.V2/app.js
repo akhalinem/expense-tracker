10adafef3ebb8cde4c76d26818f630c8a045d77d
akhalinem <akhalinem@gmail.com>
2025-08-31 16:27:59 +0500

    feat: [EXTRK-18] initialize express server with basic routing

--- ExpenseTracker.Api.V2/app.js
+++ ExpenseTracker.Api.V2/app.js
@@ -1,1 +1,11 @@
-console.log("Let's rock!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Let's rock!");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
